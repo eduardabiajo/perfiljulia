a678245c72176e78cb38090f5e57e174e73539f4
Eduarda Biajo Tavares <eduarda.bt.veleiros@gmail.com>
2024-09-24 16:36:49 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 const toggleTheme = document.getElementById("toggleTheme");
-
 const rootHtml = document.documentElement;
 
-// FUNÇÃO PARA ALTERNAR O TEMA 
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+//const menuLinks = document.querySelectorAll(".menu-link");
+
+
+
+
+//função para alternar o tema 
 function changeTheme () {
     const currentTheme = rootHtml.getAttribute("data-theme");
 
     currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
-    toggleTheme.classList.toggle("bi-sun-fill");
-    toggleTheme.classList.toggle("bi-moon-fill");
-}
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+
+} 
 
 toggleTheme.addEventListener("click", changeTheme);
+//fim da função mudar o tema
+
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
